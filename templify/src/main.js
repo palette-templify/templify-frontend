556def26212bf68b39b87649c5b9235643741c00
Donggi Yang <xronace@gmail.com>
2025-02-27 17:34:32 +0900

    (Trello: eF2djcvQ) feat: Vuetify 설치
    MUI는 react.js 전용이므로 UI kit을 Vuetify로 사용.

--- templify/src/main.js
+++ templify/src/main.js
@@ -2,8 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
+
+app.use(vuetify);
 
 app.use(createPinia());
 
