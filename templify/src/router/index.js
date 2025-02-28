c672a511ce134e54136e22df6a5f046327b2fb8c
Donggi Yang <xronace@gmail.com>
2025-02-28 17:22:51 +0900

    (Trello: IUevf9TR) feat: 로그인 상태가 아닐 시 로그인 화면으로 리다이렉션 되도록 처리 + 샘플 WriteView.vue 파일 생성

--- templify/src/router/index.js
+++ templify/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
+import WriteView from "@/views/WriteView.vue";
+import { useAuthStore } from "@/stores/auth";
 
 const routes = [
   {
@@ -7,11 +9,27 @@ const routes = [
     name: "Login",
     component: LoginView,
   },
+  {
+    path: "/write",
+    name: "Write",
+    component: WriteView,
+    meta: { requiresAuth: true },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.loggedIn) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ templify/src/views/WriteView.vue
@@ -0,0 +1,1 @@
+<template>writeView</template>
