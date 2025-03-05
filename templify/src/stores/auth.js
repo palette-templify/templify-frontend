50c68ca6a1dc2c110b91e397307de5967430d167
Donggi Yang <xronace@gmail.com>
2025-03-05 18:31:32 +0900

    (Trello: uWNcehLX) fix: 새로고침 시 로그인 화면으로 이동하는 현상 수정.

--- templify/src/stores/auth.js
+++ templify/src/stores/auth.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
-    loggedIn: false,
+    loggedIn: !!sessionStorage.getItem("accessToken"),
   }),
   actions: {
     login(userData) {
